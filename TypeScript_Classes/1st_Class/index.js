0f202f0a310e57c0f10c258145c12469dfdab39a
eDeveloper132 <1375219.aptechiic@gmail.com>
2025-07-03 02:51:09 +0500

    TypeScript Class Added

---
+++ TypeScript_Classes/1st_Class/index.js
@@ -0,0 +1,41 @@
+// Variable kya hota hai TypeScript mein?
+// Variable aik container hota hai jo kisi value (number, string, boolean, object, waghera) ko store karta hai, aur TypeScript mein uski type bhi specify kar sakte ho taake compile time pe bugs pakray jaayen.
+var age = 21;
+var namee = "Ilyas";
+var isOnline = true;
+// var, let, aur const ka farq?
+// | Keyword | Scope           | Hoisting                 | Reassignable?      | Redeclarable? |
+// | ------- | --------------- | ------------------------ | ------------------ | ------------- |
+// | `var`   | Function/global | Haan (undefined se init) | ✅ haan             | ✅ haan        |
+// | `let`   | Block (`{}`)    | Haan (TDZ tak dead zone) | ✅ haan             | ❌ nahi        |
+// | `const` | Block (`{}`)    | Haan (TDZ)               | ❌ nahi (must init) | ❌ nahi        |
+// var: Purana tareeqa JS ka. Function-scoped, hoisted, reassign aur redeclare dono ho sakte.
+// let: Block-scoped, hoisted lekin temporal dead zone mein, reassign hota lekin redeclare nahi hota.
+// const: Block-scoped, must initialize karna, reassign nahi ho sakta. (Magar object ke andar values mutate ho sakti hain.)
+// 123321234 //This is the data type Number
+// "32424324"//This is the data type String
+// true false//This is the data type Boolean
+var num = 123321234;
+var str = "32424324";
+var bool = true;
+// var data1 = "jojo";
+// console.log(data1);
+// var data1 = "jojo2";
+// console.log(data1);
+// var data1 = "jojo";
+// console.log(data1);
+// data1 = "jojo2";
+// console.log(data1);
+// var data2;
+// console.log(data2);
+// data2 = "jojo2";
+// console.log(data2);
+// Comment:
+// Duplicate let same scope mein → Error: “Cannot redeclare block‑scoped variable ‘data1’.”
+// TS safety feature.
+// let data1;
+// let data1;
+// let data1: string = "sdfsd";
+// data1 = "Ilyas";
+// console.log(data1);
+// const NAME: string = "SHOP EASY";
